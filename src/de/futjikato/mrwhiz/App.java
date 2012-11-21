03eab304901a9cf77ffeac832870f490f4ff5433
Moritz Spindelhirn <m.spindelhirn@cashiers-check.de>
2012-11-21 21:34:41 +0100

    changed state handling to allow runtime changes on game state

--- src/de/futjikato/mrwhiz/App.java
+++ src/de/futjikato/mrwhiz/App.java
@@ -1,32 +1,38 @@
 package de.futjikato.mrwhiz;
 
+import de.futjikato.mrwhiz.xml.Map;
+
 public class App {
 
 	private boolean debug = false;
 	private static App instance;
+
 	private GameStates state = GameStates.WORLDMAP;
-	
+	private GameStates nextState;
+
+	private Map nextGameMap;
+
 	/**
 	 * Main
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		// create new app instance
 		App app = new App();
 		App.instance = app;
-		
+
 		// parse arguments
-		for(String arg : args) {
-			if(arg.equals("-debug")) {
+		for ( String arg : args ) {
+			if (arg.equals("-debug")) {
 				app.enableDebug();
 			}
 		}
-		
+
 		app.loop();
 	}
-	
+
 	/**
 	 * Return the app instance
 	 * 
@@ -35,7 +41,7 @@ public class App {
 	public static App getInstance() {
 		return App.instance;
 	}
-	
+
 	/**
 	 * Return true if the app is started with debug flag
 	 * 
@@ -44,22 +50,36 @@ public class App {
 	public boolean isDebug() {
 		return this.debug;
 	}
-	
+
 	private App() {
-		
+
 	}
 
 	private void enableDebug() {
 		this.debug = true;
 	}
-	
+
+	public void setNextStep(GameStates gs) {
+		this.state = gs;
+	}
+
 	private void loop() {
-		while(this.state != null && this.state != GameStates.QUIT) {
-			// get runner
+		while (this.state != null && this.state != GameStates.QUIT) {
+			// set next step to exit state of new state
+			this.nextState = this.state.getExistState();
+
+			// start state now
 			this.state.start();
-			
-			// on exit get next state
-			this.state = this.state.getExistState();
+
+			this.state = this.nextState;
 		}
+	}
+
+	public void setNextGameMap(Map map) {
+		this.nextGameMap = map;
+	}
+
+	public Map getNextGameMap() {
+		return this.nextGameMap;
 	}
 }
