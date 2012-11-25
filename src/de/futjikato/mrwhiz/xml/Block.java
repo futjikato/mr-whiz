dd8a1bc1a6521cf21ef6efad55abd1c6cae41bdf
Moritz Spindelhirn <m.spindelhirn@cashiers-check.de>
2012-11-25 22:12:11 +0100

    starting to implement blocks

---
+++ src/de/futjikato/mrwhiz/xml/Block.java
@@ -0,0 +1,20 @@
+package de.futjikato.mrwhiz.xml;
+
+public class Block extends XmlObject {
+
+	@Override
+	public void handleValue(String currentValue) throws ObjectNoValueSupport {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addChildObj(XmlObject mapObj) throws ObjectNoChildSupport, ObjectInvalidChild {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void draw() {
+
+	}
+}
---
+++ src/de/futjikato/mrwhiz/xml/BlockCollector.java
@@ -0,0 +1,54 @@
+package de.futjikato.mrwhiz.xml;
+
+import java.util.HashMap;
+
+import de.futjikato.mrwhiz.xml.attributes.Dimensions;
+
+public final class BlockCollector extends XmlObject {
+
+	private HashMap<String, Block> areamap;
+
+	private BlockCollector instance;
+
+	private BlockCollector() {
+
+	}
+
+	public BlockCollector getInstance() {
+		if (instance == null) {
+			instance = new BlockCollector();
+		}
+		return instance;
+	}
+
+	@Override
+	public void handleValue(String currentValue) throws ObjectNoValueSupport {
+		throw new ObjectNoValueSupport();
+	}
+
+	@Override
+	public void addChildObj(XmlObject mapObj) throws ObjectNoChildSupport, ObjectInvalidChild {
+		if (mapObj instanceof Block) {
+			Block block = (Block) mapObj;
+			Dimensions dim = block.getDimensions();
+			String key = String.format("%d,%d", dim.getX(), dim.getY());
+
+			this.areamap.put(key, block);
+		}
+	}
+
+	public void drawBlocks(int bx, int by, int bw, int bh, int blocksize) {
+		// run thought all requested blocks
+		for ( int i = -20 ; i < bw ; i++ ) {
+			for ( int j = -20 ; j < bh ; j++ ) {
+				String key = String.format("%d,%d", bx + i, by + j);
+				Block block = this.areamap.get(key);
+
+				// draw area if there is one
+				if (block != null) {
+					block.draw();
+				}
+			}
+		}
+	}
+}
