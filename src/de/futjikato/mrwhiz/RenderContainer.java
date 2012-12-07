5474bf9118770a8f9bdfd0ccd66ecfcee802f146
Moritz Spindelhirn <m.spindelhirn@cashiers-check.de>
2012-12-07 08:38:39 +0100

    removed unused interface

--- src/de/futjikato/mrwhiz/RenderContainer.java
+++
@@ -1,7 +0,0 @@
-package de.futjikato.mrwhiz;
-
-import de.futjikato.mrwhiz.xml.XmlObject;
-
-public interface RenderContainer {
-	public XmlObject getObjects(int x, int y, int width, int height);
-}
