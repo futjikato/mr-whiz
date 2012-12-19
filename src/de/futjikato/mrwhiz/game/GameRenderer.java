54df74ff5a89793f60f090c1ddb2f0d57be24474
Moritz Spindelhirn <m.spindelhirn@cashiers-check.de>
2012-12-19 20:28:03 +0100

    cleaned up game renderer a bit

--- src/de/futjikato/mrwhiz/game/GameRenderer.java
+++ src/de/futjikato/mrwhiz/game/GameRenderer.java
@@ -19,15 +19,9 @@ import de.futjikato.mrwhiz.xml.XmlReader;
 import de.futjikato.mrwhiz.xml.attributes.Spawn;
 
 public final class GameRenderer extends Renderer {
-
-	protected int width = 800;
-	protected int height = 800;
-
 	private Gamemap map;
 	private GameUi ui;
 	private GamePlayer player;
-
-	public static final int BLOCKSIZE = 80;
 
 	private float viewPortX = 0.0f;
 	private float viewPortY = 0.0f;
@@ -45,12 +39,12 @@ public final class GameRenderer extends Renderer {
 		this.map = reader.getGame();
 
 		// calculate viewport block with & height
-		this.viewPortwb = (int) Math.ceil(Display.getWidth() / (double) this.map.getBlocksize().getBlocksize()) + 1;
-		this.viewPorthb = (int) Math.ceil(Display.getHeight() / (double) this.map.getBlocksize().getBlocksize()) + 1;
+		this.viewPortwb = (int) Math.ceil(Display.getWidth() / (double) this.map.getBlocksize().getValue()) + 1;
+		this.viewPorthb = (int) Math.ceil(Display.getHeight() / (double) this.map.getBlocksize().getValue()) + 1;
 
 		// init player
 		Spawn spawn = this.map.getSpawn();
-		this.player = new GamePlayer(spawn.getX(), spawn.getY(), this.map.getBlocksize().getBlocksize());
+		this.player = new GamePlayer(spawn.getX(), spawn.getY(), this.map.getBlocksize().getValue());
 
 		// init ui
 		this.ui = new GameUi(this.player);
@@ -66,18 +60,18 @@ public final class GameRenderer extends Renderer {
 
 		// render all texture areas in viewport
 		TextureAreaCollector areaCollector = TextureAreaCollector.getInstance();
-		areaCollector.draw(this.viewPortX, this.viewPortY, this.map.getBlocksize().getBlocksize());
+		areaCollector.draw(this.viewPortX, this.viewPortY, this.map.getBlocksize().getValue());
 
 		// render blocks
 		List<Block> blocks = BlockCollector.getInstance().getBlocksByBlockCoords(this.viewPortxb, this.viewPortyb, this.viewPortwb, this.viewPorthb, this.map.getBlocksize().getBlocksize());
 		for ( Block block : blocks ) {
-			block.draw(this.viewPortX, this.viewPortY, this.map.getBlocksize().getBlocksize());
+			block.draw(this.viewPortX, this.viewPortY, this.map.getBlocksize().getValue());
 		}
 
 		// render items
 		List<Item> items = ItemCollector.getInstance().getItemsByBlockCoords(this.viewPortxb, this.viewPortyb, this.viewPortwb, this.viewPorthb, this.map.getBlocksize().getBlocksize());
 		for ( Item item : items ) {
-			item.draw(this.viewPortX, this.viewPortY, this.map.getBlocksize().getBlocksize());
+			item.draw(this.viewPortX, this.viewPortY, this.map.getBlocksize().getValue());
 		}
 
 		this.player.render(this.viewPortX, this.viewPortY);
@@ -117,7 +111,7 @@ public final class GameRenderer extends Renderer {
 
 		// TODO check if we can center camera or if we need to add/sub an offset
 
-		this.viewPortxb = (int) Math.floor(this.viewPortX / this.map.getBlocksize().getBlocksize());
-		this.viewPortyb = (int) Math.floor(this.viewPortY / this.map.getBlocksize().getBlocksize());
+		this.viewPortxb = (int) Math.floor(this.viewPortX / this.map.getBlocksize().getValue());
+		this.viewPortyb = (int) Math.floor(this.viewPortY / this.map.getBlocksize().getValue());
 	}
 }
