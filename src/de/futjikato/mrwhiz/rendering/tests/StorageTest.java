1fe879298b787ef185c60f68e3e8bd075e79a64a
Moritz Spindelhirn <m.spindelhirn@cashiers-check.de>
2013-04-20 00:13:08 +0200

    added test draft for image storage implementation

--- src/de/futjikato/mrwhiz/rendering/tests/StorageTest.java
+++ src/de/futjikato/mrwhiz/rendering/tests/StorageTest.java
@@ -1,5 +1,13 @@
 package de.futjikato.mrwhiz.rendering.tests;
 
+import de.futjikato.mrwhiz.App;
+import de.futjikato.mrwhiz.rendering.Boundary;
+import de.futjikato.mrwhiz.rendering.ImageStorage;
+import de.futjikato.mrwhiz.rendering.Storage;
+import org.junit.*;
+
+import java.io.File;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Moritz
@@ -8,4 +16,25 @@ package de.futjikato.mrwhiz.rendering.tests;
  * To change this template use File | Settings | File Templates.
  */
 public class StorageTest {
+
+    public Storage imgStorage;
+
+    public Boundary bound;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        App.getInstance().enableUnittestMode();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        imgStorage = new ImageStorage(new File("resources/data/level/l1.png"));
+
+        bound = new Boundary(null, 0, 149, 10, 50);
+    }
+
+    @Test
+    public void testPlayerTakeDamage() {
+        imgStorage.getStructures(bound);
+    }
 }
